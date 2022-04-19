a867bb0c4ad3dd5f8e6b48deb8cf001f485c5c5c
Furkan Demir <frndemir@gmail.com>
2022-04-19 21:26:20 +0300

    :lipstick: add disable style to button

--- src/components/lib/Button/index.tsx
+++ src/components/lib/Button/index.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   return (
     <button
       type="button"
-      className="ml-auto text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className="ml-auto text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:opacity-40 disabled:hover:bg-emerald-700"
       {...props}
     >
       {children}
